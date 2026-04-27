79d7190fec83d209524b3196dc474831064b8cf1
LuisOzParr <lgozuna@gmail.com>
2026-04-27 02:58:25 -0600

    Add SettingSeeder and enforce dynamic password rules based on settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ActionRoleSeeder::class,
             SettingValueTypeSeeder::class,
+            SettingSeeder::class,
         ]);
     }
 }
